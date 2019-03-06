223f3992a42777c36595d80319915acd93e17368
Ubuntu <ubuntu@ip-172-31-13-191.us-west-1.compute.internal>
2019-03-06 07:17:47 +0000

    Serve FE

---
+++ test_fe/src/components/containers/LandingPage.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+// import './LandingPage.css'
+
+export default class LandingPage extends React.Component {
+    render() {
+      return (
+        <div>
+            <Button variant="light" style={{float:"right", padding: 5+"px",
+            margin: 10+"px"}}>Sign Up</Button>
+            <h1>Streamosphere</h1>
+            <Form class="signInForm" style={{width:512+"px", padding: 16+"px"}}>
+                <h2 >Sign In</h2>
+                <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="formPlaintextPassword">
+                    <Form.Label column sm="2">
+                    Password
+                    </Form.Label>
+                    <Col sm="10">
+                    <Form.Control type="password" placeholder="Password" />
+                    </Col>
+                </Form.Group>
+            </Form>
+        </div>
+      );
+    }
+  }
+
