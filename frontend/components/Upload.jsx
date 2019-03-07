53eff77b3445b3d7896c11db2c0a49a85f2d3fe0
Khoa100 <minhkhoavu954@gmail.com>
2019-03-07 13:32:39 -0800

    added my files

---
+++ frontend/components/Upload.jsx
@@ -0,0 +1,16 @@
+﻿import React, { Component } from 'react';
+
+export default class Upload extends Component {
+    upload() {
+        document.getElementById("newFile").click();
+    }
+    render() {
+        return (
+            <div>
+                <button id="new" onClick={this.upload}>Upload</button>
+                <input id="newFile" hidden type="file" />
+            </div>
+        );
+    }
+
+}
