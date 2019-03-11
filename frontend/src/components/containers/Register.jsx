65c409bdcbd8ec6fc1f8ac5901cd7b2d9c979576
Khoa100 <minhkhoavu954@gmail.com>
2019-03-10 21:16:48 -0700

    added registration

---
+++ frontend/src/components/containers/Register.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import sph from '../images/accImage.jpg';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+
+export class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            pass: ''
+        };
+
+        
+    }
+    handleChange(event) {
+        this.setState({
+            username: event.target.value,
+            pass: event.target.value
+        })
+    }
+    handleSubmit(data) {
+        this.username = data.input.username,
+        this.pass = data.input.pass
+    }
+    
+    
+    render() {
+        return (
+            <div>
+                <img src={sph} alt="a sphere" wdith="300" height="300" />
+				<Form>
+					<Form.Group controlId="exampleForm.ControlInput1">
+						<Form.Label>Email address </Form.Label>
+						<Form.Control type="email" placeholder="name@example.com" />
+						<Form.Label>Password</Form.Label>
+						<Form.Control type="password" placeholder="Password" />
+					</Form.Group>
+				</Form>
+				<Nav.Link href="/AccountHome"><Button type="Submit">Submit</Button></Link>
+            </div>
+        );
+    }
+}
