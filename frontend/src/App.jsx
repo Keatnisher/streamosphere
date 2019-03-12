86ce023707164a9c2456da5ab4d160a957c84adb
Khoa100 <minhkhoavu954@gmail.com>
2019-03-11 21:33:03 -0700

    included link to registration page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
-import LandingPage from './components/containers/LandingPage.jsx'
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
+import LandingPage from './components/containers/LandingPage.jsx';
+import Register from './components/containers/Register.jsx';
 class App extends React.Component {
     render() {
       return (
         <Router history={hashHistory}>
           <Route path='/' component={LandingPage} />
+          <Route path='/register' component={Register} />
         </Router>
       );
     }
