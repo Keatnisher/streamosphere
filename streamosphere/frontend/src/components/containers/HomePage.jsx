c8190cdf6bdfe77656aed3d523c1ab57f5d1fad9
Khoa100 <minhkhoavu954@gmail.com>
2019-04-11 00:01:45 -0700

    Update HomePage.jsx

--- streamosphere/frontend/src/components/containers/HomePage.jsx
+++ streamosphere/frontend/src/components/containers/HomePage.jsx
@@ -1,7 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import '../layouts/MainLayout.css';
+import sph from '../../images/sphere.jpg';
+import sph1 from '../../images/csSphere.jpg';
 
 const HomePage = () => (
     <div>
-        <h1>Home Page</h1>
+        <Link to="/login"><button>Login</button></Link> <br /> 
+        <h1>Welcome to Streamosphere!</h1>
+        <img src={sph} alt="a gray sphere" width="400" height="400" />
+        <img src={sph1} alt="a sphere of binary digits" width="400" height="400" />
     </div>
 );
 
