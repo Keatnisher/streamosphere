89a0aad36becaabde33c5799ffb152ad55c59d88
Khoa100 <minhkhoavu954@gmail.com>
2019-05-10 10:38:49 -0700

    added comments

--- frontend/src/components/containers/DropZone.jsx
+++ frontend/src/components/containers/DropZone.jsx
@@ -1,13 +1,16 @@
-﻿import React, { Component } from "react";
+//external resources to be imported
+import React, { Component } from "react";
 import "../../layouts/Dropzone.css";
 import UploadIcon from '../../images/upload-to-cloud-26px.png';
 
+//this component is where the user will upload a file from their client
 class DropZone extends Component {
     constructor(props) {
         super(props);
         this.state = { hightlight: false };
-        this.fileInputRef = React.createRef();
+        this.fileInputRef = React.createRef(); //reference object
 
+        //functions needed for DropZone component
         this.openFileDialog = this.openFileDialog.bind(this);
         this.onFilesAdded = this.onFilesAdded.bind(this);
         this.onDragOver = this.onDragOver.bind(this);
@@ -15,11 +18,13 @@ class DropZone extends Component {
         this.onDrop = this.onDrop.bind(this);
     }
 
+    //get file selected from the client
     openFileDialog() {
         if (this.props.disabled) return;
         this.fileInputRef.current.click();
     }
-
+    
+    //add uploaded file to array
     onFilesAdded(evt) {
         if (this.props.disabled) return;
         const files = evt.target.files;
@@ -28,17 +33,20 @@ class DropZone extends Component {
             this.props.onFilesAdded(array);
         }
     }
-
+    
+    //user drags a file to the component
     onDragOver(event) {
         event.preventDefault();
         if (this.props.disabed) return;
         this.setState({ hightlight: true });
     }
-
+    
+    //mouse is out of component
     onDragLeave(event) {
         this.setState({ hightlight: false });
     }
 
+    //mouse is on component
     onDrop(event) {
         event.preventDefault();
         if (this.props.disabed) return;
@@ -49,7 +57,8 @@ class DropZone extends Component {
         }
         this.setState({ hightlight: false });
     }
-
+    
+    //convert list to array
     fileListToArray(list) {
         const array = [];
         for (var i = 0; i < list.length; i++) {
@@ -57,7 +66,8 @@ class DropZone extends Component {
         }
         return array;
     }
-
+    
+    //DropZone view component
     render() {
         return (
             <div
